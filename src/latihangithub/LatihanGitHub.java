4a51dc544f6472a200a788a536710918d58746ea
ustore <ustore@192.168.43.238>
2018-11-21 09:59:03 +0700

    adding yuda name

---
+++ src/latihangithub/LatihanGitHub.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihangithub;
+
+/**
+ *
+ * @author Nitani
+ */
+public class LatihanGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Bootcamp 21");
+        System.out.println("Andi Selamet R");
+        System.out.println("Mukhlis Purnomo Aji");
+        System.out.println("ainanichan is here..");
+        System.out.println("Adhe Tiga");
+        System.out.println("Igaz F");
+        System.out.println("Fathur");
+        System.out.println("Tika Mahardhika P");
+    }
+    
+   
+}
